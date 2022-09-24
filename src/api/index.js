0fc8f58f82f079cc7f7c8a34beff3f0f55375fbc
tuan <nhoxtuanhero1@gmail.com>
2022-09-24 18:07:20 +0700

    fix error

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,2 @@
 export const URLDatabase =
   "https://server-test-4zlw-mv29zk34a-tuankun-11062001.vercel.app";
-
-// api post
-// export const fetchAPICreatePost = (data) => {
-//   console.log(data);
-//   return axios.post(`${URLDatabase}/posts/createPost`, data);
-// };
